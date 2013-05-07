4c99352bbb43c357fbcc847fbb6d986e63b65e59
Sebastien Windal <swindal@wgen.net>
2013-05-07 15:10:19 -0400

    removing logs

--- web.js
+++ web.js
@@ -1,22 +1,17 @@
 var static = require('node-static');
 
-console.log("I am starting"); 
 //
 // Create a node-static server instance to serve the './app' folder
 //
 var file = new(static.Server)('./app');
-console.log("1");
 var port = process.env.PORT || 5000;
-console.log("2");
-require('http').createServer(function (request, response) {
-	console.log("3");
+var http = require('http');
+
+http.createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
         //
-console.log("4");
         file.serve(request, response);
-console.log("5");
     });
 }).listen(port);
-console.log("6");
