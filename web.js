15d20930635e3fb1d79a6ae6f96a540a65f46852
Sebastien Windal <swindal@wgen.net>
2013-05-07 15:15:12 -0400

    simplification

--- web.js
+++ web.js
@@ -8,10 +8,5 @@ var port = process.env.PORT || 5000;
 var http = require('http');
 
 http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
         file.serve(request, response);
-    });
 }).listen(port);
