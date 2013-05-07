5550e981175c29670f197b6102246f44b6f534dd
Sebastien Windal <swindal@wgen.net>
2013-05-07 15:20:15 -0400

    adding version

--- web.js
+++ web.js
@@ -8,5 +8,5 @@ var port = process.env.PORT || 5000;
 var http = require('http');
 
 http.createServer(function (request, response) {
-        file.serve(request, response);
+    file.serve(request, response);
 }).listen(port);
