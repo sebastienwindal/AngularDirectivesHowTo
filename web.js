5a5074b1bdd3a2f2df792693a79f8f3ef1a7f2da
Sebastien Windal <swindal@wgen.net>
2013-05-07 15:02:59 -0400

    debug logs

--- web.js
+++ web.js
@@ -5,12 +5,17 @@ console.log("I am starting");
 // Create a node-static server instance to serve the './app' folder
 //
 var file = new(static.Server)('./app');
+console.log("1");
 var port = process.env.PORT || 5000;
+console.log("2");
 require('http').createServer(function (request, response) {
+	console.log("3");
     request.addListener('end', function () {
         //
         // Serve files!
         //
+console.log("4");
         file.serve(request, response);
+console.log("5");
     });
 }).listen(port);
